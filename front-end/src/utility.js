6ba034c83462ba9795cd6193cc3ac0908f94faa5
Shasika Udayanga <shasikaudayanga@gmail.com>
2024-04-14 19:54:09 +0100

    added ingredients

--- front-end/src/utility.js
+++ front-end/src/utility.js
@@ -16,6 +16,60 @@ const foodItems = [
         "protein": 1.15,
         "carbs": 28.96,
         "fat": 0.26
+    },
+    {
+        "key": "Orange",
+        "protein": 0.79,
+        "carbs": 11.79,
+        "fat": 0.23
+    },
+    {
+        "key": "Carrots",
+        "protein": 1.19,
+        "carbs": 12.26,
+        "fat": 0.31
+    },
+    {
+        "key": "Broccoli",
+        "protein": 2.57,
+        "carbs": 6.04,
+        "fat": 0.34
+    },
+    {
+        "key": "Tomato",
+        "protein": 1.58,
+        "carbs": 7.06,
+        "fat": 0.36
+    },
+    {
+        "key": "Beef",
+        "protein": 14.2,
+        "carbs": 0,
+        "fat": 10.4
+    },
+    {
+        "key": "Chicken",
+        "protein": 16,
+        "carbs": 0,
+        "fat": 1.84
+    },
+    {
+        "key": "Egg",
+        "protein": 6.29,
+        "carbs": 0.38,
+        "fat": 4.97
+    },
+    {
+        "key": "Rice 1 Cup",
+        "protein": 4.2,
+        "carbs": 44.08,
+        "fat": 0.44
+    },
+    {
+        "key": "Beer",
+        "protein": 1.64,
+        "carbs": 12.64,
+        "fat": 0
     }
 ];
 //https://www.calculator.net/macro-calculator.html
